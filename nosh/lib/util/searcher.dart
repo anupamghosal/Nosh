1b0cf77c36a19ad9ba643889cc76690a218444ed
SumanX22 <suman.mesaha22@gmail.com>
2020-04-18 01:03:02 +0530

    search implemented

--- nosh/lib/util/searcher.dart
+++ nosh/lib/util/searcher.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import '../database/stockItem.dart';
+
 class searchItems extends SearchDelegate {
+  final List<StockItem> items;
+
+  searchItems(this.items);
+
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -26,13 +34,45 @@ class searchItems extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    return Container();
+    final reults =
+        items.where((a) => a.getName().toLowerCase().contains(query));
+    return ListView(
+      children: reults
+          .map<ListTile>((a) => ListTile(
+                leading: Icon(Icons.fastfood),
+                title: Text(
+                  a.getName(),
+                ),
+                subtitle: Text(
+                  a.getExpiryDate() == ""
+                      ? "No expiry mentioned"
+                      : a.getExpiryDate().toString(),
+                ),
+                onTap: () => close(context, a),
+              ))
+          .toList(),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    return Container();
+    final suggestions =
+        items.where((a) => a.getName().toLowerCase().contains(query));
+    return ListView(
+      children: suggestions
+          .map<ListTile>((a) => ListTile(
+                title: Text(
+                  a.getName(),
+                  style: TextStyle(color: Colors.grey[800]),
+                ),
+                subtitle: Text(
+                  a.getExpiryDate() == ""
+                      ? "No expiry mentioned"
+                      : a.getExpiryDate().toString(),
+                ),
+                onTap: () => close(context, a),
+              ))
+          .toList(),
+    );
   }
 }
